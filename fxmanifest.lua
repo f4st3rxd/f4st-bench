be49ae90097703862d579740036ba1002a3cddd5
f4st3r <152923524+f4st3rxd@users.noreply.github.com>
2024-08-24 07:03:49 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'f4st3r'
+description 'job bench'
+lua54 "yes"
+
+client_scripts {"client/*.lua"}
+server_scripts {"server/*.lua"}
+shared_scripts {"config.lua"}
